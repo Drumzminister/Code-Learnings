bb621ba4a9ac4f46c533061e7821c7e67111dc4b
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2017-10-26 21:27:44 +0100

    mobile header responsiveness updated

---
+++ header.php
@@ -0,0 +1,29 @@
+<!-- Navbar -->
+<nav class="navbar navbar-inverse navbar-fixed-top" style="background-color: #0F3B75;">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#HumanATMNavbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a id="header-color" class="navbar-brand" href="#">
+                <img style="width:45px;height:45px;padding:5px;" src="img/logo.png" alt="">
+                Human ATM</a>
+        </div>
+        <div class="collapse navbar-collapse" id="HumanATMNavbar">
+            <ul class="nav navbar-nav navbar-right" id="header-color" >
+                <li><a href="#">WALLET</a></li>
+                <li><a href="#">TRANSACTIONS</a></li>
+                <li><a href="#">PAY BILLS</a></li>
+                <li><a href="#" >INVITE FRIENDS</a></li>
+            </ul>
+        </div>
+    </div>
+</nav><!-- Navbar Ends -->
+
+<!-- Put the body content here -->
+
+
+
